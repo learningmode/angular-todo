81c3b9c567c55f0123e362a5f01f75bc6705a3a8
learningmode <learningmode151@gmail.com>
2021-08-05 18:10:38 +0530

    Added button functionality with rxjs

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 import {Task} from '../../Task';
 
 
@@ -12,11 +14,16 @@ export class AddTaskComponent implements OnInit {
   text:string;
   day:string;
   reminder:boolean;
-  constructor() {
+  subscription:Subscription;
+  showAddTask:boolean;
+
+  constructor(private uiService:UiService) {
     this.text =" ";
     this.day =" ";
     this.reminder = false;
+    this.showAddTask= false;
     this.onAddTask=new EventEmitter();
+    this.subscription = this.uiService.onToggle().subscribe(value=>this.showAddTask=value);
    }
 
   ngOnInit(): void {
